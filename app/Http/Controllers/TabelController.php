2f1471b99dd6e160a218d4297ca110c892ef0a47
Muiz Muharram <72422975+Muiz021@users.noreply.github.com>
2023-06-25 15:14:23 +0800

    penambahan fitur pdf pada dosen pembimbing

--- app/Http/Controllers/TabelController.php
+++ app/Http/Controllers/TabelController.php
@@ -26,7 +26,7 @@ class TabelController extends Controller
             $kegiatan = Kegiatan::where('mahasiswa_id', $mahasiswa->id)->orderBy('tanggal', 'asc')->get();
             $nama = Str::slug($mahasiswa->nama, '_'); // Mengganti spasi dengan tanda -
             $fileName = "tabel_kegiatan_ppl_{$nama}.pdf";
-            $baseURL = url('/'). '/pdf/' . $fileName;
+            $baseURL = url('/') . '/pdf/' . $fileName;
             if ($mahasiswa->where('pdf', null)) {
                 $mahasiswa->update([
                     'pdf' => $baseURL
@@ -54,12 +54,42 @@ class TabelController extends Controller
         $user = Auth::user();
         if ($user->roles == 'dosen_pembimbing') {
             $dosen_pembimbing = DosenPembimbing::where('user_id', $user->id)->first();
+            $seluruhMahasiswa = Mahasiswa::where('dosen_pembimbing_id', $dosen_pembimbing->id)->count();
             $mahasiswa = Mahasiswa::where('dosen_pembimbing_id', $dosen_pembimbing->id)->whereNotNull('pdf')->get();
-            $pdf = PDF::loadView('Admin.pages.table.seluruh-kegiatan-mahasiswa', ['mahasiswa' => $mahasiswa]);
-            $pdf->setPaper('A4', 'portrait');
-            $fileName = "tabel_kegiatan_ppl_dosen_{$dosen_pembimbing->nama}.pdf";
 
-            return $pdf->stream($fileName);
+            if ($seluruhMahasiswa === $mahasiswa->count()) {
+                $nama = Str::slug($dosen_pembimbing->nama, '_'); // Mengganti spasi dengan tanda -
+                $fileName = "tabel_kegiatan_ppl_dosen_{$nama}.pdf";
+                $baseURL = url('/') . '/pdf/' . $fileName;
+
+                if ($dosen_pembimbing->pdf === null) {
+                    $dosen_pembimbing->update([
+                        'pdf' => $baseURL
+                    ]);
+
+                    $pdf = PDF::loadView('Admin.pages.table.seluruh-kegiatan-mahasiswa', ['mahasiswa' => $mahasiswa]);
+                    $pdf->setPaper('A4', 'portrait');
+                    $filePath = public_path('pdf/' . $fileName);
+                    $pdf->save($filePath);
+                    $pdf->download("{$fileName}");
+
+                    return response()->json([
+                        "message" => "url file kegiatan pdf mahasiswa pada dosen pembimbing",
+                        "data" => $dosen_pembimbing->pdf
+                    ]);
+                }else{
+                    return response()->json([
+                        "message" => "file sudah tersedia",
+                        "data" => $dosen_pembimbing->pdf
+                    ]);
+                }
+            } else {
+                return response()->json([
+                    "message" => "masih ada mahasiswa yang belum generate file kegiatan ppl",
+                    "data" => null
+                ]);
+            }
         }
+
     }
 }
